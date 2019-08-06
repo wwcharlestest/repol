eb029f52ca9632c720efd5cd10b876e68815afe2
wuwenjun <1719097673@qq.com>
2019-08-06 09:49:27 +0800

    this is today working

---
+++ exam03.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# if statement I
+
+if true ; then
+echo  "this is true"
+fi
---
+++ exam04.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# if statement I
+
+if  ls /etci > /dev/null; then
+echo "etc directory is OK"
+fi
